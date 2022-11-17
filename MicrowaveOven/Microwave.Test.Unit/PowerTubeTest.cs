cfc3b2a904111cd32c199d38cd291025ee4debae
Nivethan1999 <m-iquaz@hotmail.com>
2022-11-17 11:07:47 +0100

    4 new tests added to PowerTubeTest where we test different powertube configrations

--- MicrowaveOven/Microwave.Test.Unit/PowerTubeTest.cs
+++ MicrowaveOven/Microwave.Test.Unit/PowerTubeTest.cs
@@ -24,7 +24,7 @@ namespace Microwave.Test.Unit
         [TestCase(100)]
         [TestCase(999)]
         [TestCase(1000)]
-        public void TurnOn_WasOffCorrectPower_CorrectOutput(int power)
+        public void TurnOn_WasOffCorrectPower_CorrectOutput1000Maximumpower(int power)
         {
             uut.TurnOn(power);
             output.Received().OutputLine(Arg.Is<string>(str => str.Contains($"{power}")));
@@ -35,10 +35,57 @@ namespace Microwave.Test.Unit
         [TestCase(0)]
         [TestCase(1001)]
         [TestCase(1050)]
-        public void TurnOn_WasOffOutOfRangePower_ThrowsException(int power)
+        public void TurnOn_WasOffOutOfRangePower_ThrowsException1000Maximumpower(int power)
         {
             Assert.Throws<System.ArgumentOutOfRangeException>(() => uut.TurnOn(power));
         }
+
+        [TestCase(1)]
+        [TestCase(50)]
+        [TestCase(100)]
+        [TestCase(499)]
+        [TestCase(500)]
+        public void TurnOn_WasOffCorrectPower_CorrectOutput500Maximumpower(int power)
+        {
+            var maximumpower500 = new PowerTube(output, 500); // Testing other power tube configuration maximum power instead of hard coded value in setup
+            maximumpower500.TurnOn(power);
+            output.Received().OutputLine(Arg.Is<string>(str => str.Contains($"{power}")));
+        }
+
+        [TestCase(-5)]
+        [TestCase(-1)]
+        [TestCase(0)]
+        [TestCase(501)]
+        [TestCase(550)]
+        public void TurnOn_WasOffOutOfRangePower_ThrowsException500Maximumpower(int power)
+        {
+            var maximumpower500 = new PowerTube(output, 500);
+            Assert.Throws<System.ArgumentOutOfRangeException>(() => maximumpower500.TurnOn(power));
+        }
+
+        [TestCase(1)]
+        [TestCase(50)]
+        [TestCase(100)]
+        [TestCase(799)]
+        [TestCase(800)]
+        public void TurnOn_WasOffCorrectPower_CorrectOutput800Maximumpower(int power)
+        {
+            var maximumpower800 = new PowerTube(output, 800); // Testing other power tube configuration maximum power instead of hard coded value in setup uut
+            maximumpower800.TurnOn(power);
+            output.Received().OutputLine(Arg.Is<string>(str => str.Contains($"{power}")));
+        }
+
+        [TestCase(-5)]
+        [TestCase(-1)]
+        [TestCase(0)]
+        [TestCase(801)]
+        [TestCase(850)]
+        public void TurnOn_WasOffOutOfRangePower_ThrowsException800Maximumpower(int power)
+        {
+            var maximumpower800 = new PowerTube(output, 800);
+            Assert.Throws<System.ArgumentOutOfRangeException>(() => maximumpower800.TurnOn(power));
+        }
+
 
         [Test]
         public void TurnOff_WasOn_CorrectOutput()
