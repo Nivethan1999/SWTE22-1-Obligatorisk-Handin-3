401e0d7d3788ac388eff43b687b62806b0c69594
Nivethan1999 <m-iquaz@hotmail.com>
2022-11-21 13:13:12 +0100

    Revert "Fixed Coverage"
    This reverts commit 20adf59d25102c32438693e9ac21c7e81657c5ab.

--- MicrowaveOven/Microwave.Classes/Controllers/CookController.cs
+++ MicrowaveOven/Microwave.Classes/Controllers/CookController.cs
@@ -70,6 +70,9 @@ namespace Microwave.Classes.Controllers
             }
         }
 
-        
+        public void addTimer(int time)
+        {
+             myTimer.TimeRemaining += time;
+        }
     }
 }
